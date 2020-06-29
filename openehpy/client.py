9bd918b6f7c9cb5dd20fccd18336b90c7771957f
Adrian Sølvberg <adso@dips.no>
2020-06-29 11:18:16 +0200

    updated readme and added support for headers

--- openehpy/client.py
+++ openehpy/client.py
@@ -2,13 +2,19 @@ import requests
 import pandas
 
 class server_connection:
-    def __init__(self, EHRStoreURL, responseEncoding = 'utf-8-sig', verifySSLConnection = True):
+    def __init__(self, EHRStoreURL, responseEncoding = 'utf-8-sig',headers = None, verifySSLConnection = True):
         self.EHRStoreURL = EHRStoreURL
         self.responseEncoding = responseEncoding
         self.verifySSLConnection = verifySSLConnection
+        self.headers = headers
 
     def query(self, query):
-        response = requests.post(self.EHRStoreURL + '/openehr/v1/query/aql/', json = { 'q': query }, verify = self.verifySSLConnection)
+        response = requests.post(
+          self.EHRStoreURL + '/openehr/v1/query/aql/',
+          json = { 'q': query },
+          verify = self.verifySSLConnection,
+          headers = self.headers
+          )
         response.encoding = self.responseEncoding
         return self.ResultsetAsDataFrame(response.json()) if response.ok else response
 
