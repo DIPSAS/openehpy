69ede3a4a2b0a42b942a90853212dd9e1a3180ee
Adrian Sølvberg <adso@dips.no>
2020-06-29 14:06:18 +0200

    removed line

--- tests/testregex.py
+++ tests/testregex.py
@@ -17,7 +17,6 @@ class Test_regex(unittest.TestCase):
             composition c
             contains observation o[openEHR-EHR-OBSERVATION.blood_pressure.v1]"""
         sanitized = """select o/data[at0001]/events[at0006]/time/value as datetime, o/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value/magnitude AS systolic, o/data[at0001]/events[at0006]/data[at0003]/items[at0005]/value/magnitude AS diastolic from composition c contains observation o[openEHR-EHR-OBSERVATION.blood_pressure.v1]"""
-        test = client.server_connection.remove_control_characters(unsanitized)
         self.assertEqual(client.server_connection.remove_control_characters(unsanitized), sanitized)
 if __name__ == '__main__':
     unittest.main()
